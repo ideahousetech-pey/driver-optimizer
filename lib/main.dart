4f9b488decfba8131533563d6a824c9c81a4b013
idea house <ideahouse.tech@gmail.com>
2026-04-27 21:06:54 +0700

    fix: resolve merge conflict in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,6 @@ void main() async {
   };
 
   runApp(const DriverOptimizerApp());
-<<<<<<< HEAD
-
-  Future.delayed(const Duration(seconds: 1), () {
-    BackgroundService.initialize();
-  });
-=======
->>>>>>> 23b3b788b8d61b37d4a80f58fefae92992f8944e
 }
 
 class DriverOptimizerApp extends StatelessWidget {
